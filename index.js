2f2d913fe8006a3a4e48d65c676d8e7fd9e7ca91
roberto14118927 <robertoruiz141189@gmail.com>
2019-02-13 12:08:19 -0600

    primer

--- index.js
+++ index.js
@@ -1,90 +1,48 @@
-var express = require('express');
-var hex2ascii = require('hex2ascii');
-var app = express();
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header("Access-Control-Allow- Headers", "Content-Type");
-    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
-    next();
-});
+var express = require('express')
+var aplicacion = express()
 
-const net = require('net');
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
-const os = require('os');
+const net = require('net')
+const server = require('http').Server(aplicacion)
+const socket = require('socket.io')(server)
+const os = require('os')
 
 var interfaces = os.networkInterfaces();
 var addresses = [];
-for (var k in interfaces) {
-    for (var k2 in interfaces[k]) {
+var d;
+for (var k in interfaces){
+    for(var k2 in interfaces[k]){
         var address = interfaces[k][k2];
-        if (address.family === 'IPv4' && !address.internal) {
-            addresses.push(address.address);
+        if (address.family == 'IPv4' && !address.internal){
+            d = address.address.toString()
+            // console.log(address.address)
+            // addresses.push(address.address);
         }
     }
 }
 
-var HOST = addresses[0];
+
+var HOST = d;
 var PORT = 3000;
+var sockets = [];
+console.log(HOST);
+var ser = net.createServer(function(so){
 
+    // sockets[0] = so;
+    // socket.write(sockets[0]);
+    // console.log(so)
+    // console.log(so)
+    console.log('Usuario conectado' + so.remoteAddress+':'+so.remotePort)
+    // so.on('connect', function(){
+    //     console.log('Usuario Nuevo')
+    // })
 
-server.listen(PORT, function () {
-    console.log("Servidor corriendo puerto: " + PORT + ", " + HOST)
+    so.on('data', function(data){
+        console.log(data)
+    })
+
+    so.on('close', function(){
+        console.log('Usuario Desconectado')
+    })
 });
 
-
-
-
-var ESP32 = net.createServer(function (sock) {
-    sock.on('data', function (data) {
-        console.log(data);
-        // console.log(data);
-        // datosin = data.toString().split(".");
-        // if (datosin.length == 0) {
-        //     return;
-        // }
-        // console.log("CONECTADO: " + Object.keys(esp_sockets).length);
-    });
-
-    // sock.on('close', function (data) {
-    //     console.log("close...");
-    //     var idx = esp_sockets.indexOf(sock);
-    //     if (idx != -1) {
-    //         esp_sockets.splice(idx, 1);
-    //     }
-
-    // });
-
-    // sock.on('end', function () {
-    //     var idx = esp_sockets.indexOf(sock);
-    //     if (idx != -1) {
-    //         esp_sockets.splice(idx, 1);
-    //     }
-    //     console.log("end...");
-    // });
-
-    // sock.on('timeout', function (data) {
-    //     console.log("timeout...");
-    // });
-
-    // sock.on('error', function (data) {
-    //     console.log("error...");
-    // });
-
-});
-
-
-
-ESP32.on('error', function(e) { 
-    console.log("Error: Necesario reiniciar...");
-    if (e.code == 'EADDRINUSE') {
-      console.log('Address in use, retrying...');
-      setTimeout(function() {
-        ESP8266.close();
-        ESP8266.listen(PORT, PORT);
-      }, 1000);
-    }
-  });
-
-  ESP32.listen(PORT, HOST);
+ser.listen(PORT, HOST);
